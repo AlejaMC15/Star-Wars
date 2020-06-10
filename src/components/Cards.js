9b3e8d2ea47328178c30de754eae2630942b6bef
AlejaMC15 <aleja-mc@hotmail.com>
2020-06-09 20:55:07 -0500

    Se agregó la Info de cada personaje

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './cards.css';
 import axios from 'axios';
-import Luke from './images/luke-skywalker.jpg';
-import ContadorComponent from './Contador1';
-import ContadorComponent2 from './Contador2';
+import ContadorComponent from './Contador';
+import Like from './images/like.png';
+import DontLike from './images/dontlike.png';
 
 class Cards extends Component {
   constructor(props) {
@@ -31,30 +31,81 @@ class Cards extends Component {
     return (
       <div className="body">
         <div>
-          {this.state.people.map((people) => (
-            <div className="cards">
-              <div className="container-fluid d-flex justify-content-center mr-5 p-3">
-                <div className="row">
-                  <div className="col-sm-12 col-md-12 col-lg-12 p-0">
-                    <div className="card" style={{ width: '18rem' }}>
-                      <img src={Luke} className="card-img-top" alt="..." />
-                      <div className="card-body">
-                        <h5 className="card-title text-center">
-                          {people.name}
-                        </h5>
-                        <p className="card-text"></p>
-                        <p className="card-text"></p>
-                        <div className="d-flex justify-content-around">
-                          <ContadorComponent />
-                          <ContadorComponent2 />
+          {this.state.people.map((people, index) => {
+            const like = `like-card-${index}`;
+            const dontLike = `dontLike-card-${index}`;
+            return (
+              <div className="cards">
+                <div className="container-fluid d-flex justify-content-center mr-5 p-3">
+                  <div className="row">
+                    <div className="col-sm-12 col-md-12 col-lg-12 p-0">
+                      <div className="card" style={{ width: '18rem' }}>
+                        <div className="card-body">
+                          <h5 className="card-title text-center">
+                            {people.name}
+                          </h5>
+                          <ul>
+                            <li>
+                              <p className="card-text">
+                                Birth Year: {people.birth_year}
+                              </p>
+                            </li>
+                            <li>
+                              <p className="card-text">
+                                Eyes Color: {people.eye_color}
+                              </p>
+                            </li>
+                            <li>
+                              <p className="card-text">
+                                Gender: {people.gender}
+                              </p>
+                            </li>
+                            <li>
+                              <p className="card-text">
+                                Hair Color: {people.hair_color}
+                              </p>
+                            </li>
+                            <li>
+                              <p className="card-text">
+                                Height: {people.height}
+                              </p>
+                            </li>
+                            <li>
+                              <p className="card-text">Mass: {people.mass}</p>
+                            </li>
+                            <li>
+                              <p className="card-text">
+                                Skin Color: {people.skin_color}
+                              </p>
+                            </li>
+                            <li>
+                              <p className="card-text">
+                                Created: {people.created}
+                              </p>
+                            </li>
+                            <li>
+                              <p className="card-text">
+                                Edited: {people.edited}
+                              </p>
+                            </li>
+                          </ul>
+
+                          <div className="d-flex justify-content-around">
+                            <ContadorComponent storageKey={like}>
+                              <img className="like" src={Like} alt="Like" />
+                            </ContadorComponent>
+                            <ContadorComponent storageKey={dontLike}>
+                              <img className="like" src={DontLike} alt="Like" />
+                            </ContadorComponent>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
         <nav aria-label="Page navigation example p-3">
           <ul class="pagination justify-content-center">
